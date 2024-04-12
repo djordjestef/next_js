9e959b18adb581c0345cb44e4f7a4127fe0f1a56
djordje <djordje.stefanovic89@gmail.com>
2024-04-12 13:23:38 +0200

    post, delete on mongoDb

--- src/lib/action.js
+++ src/lib/action.js
@@ -1,4 +1,42 @@
-export const addPost = async(formData)=>{
-    'use server';
-    console.log('formData',formData)
-}
+"use server";
+import { revalidatePath } from "next/cache";
+import { Post } from "./models";
+import { connectToDb } from "./utils";
+
+export const addPost = async (formData) => {
+  console.log("formData", formData);
+  console.log(Object.fromEntries(formData));
+  const { title, desc, slug, userId } = Object.fromEntries(formData);
+  console.log("title", title);
+  try {
+    connectToDb();
+    const newPost = new Post({
+      title,
+      desc,
+      slug,
+      userId,
+    });
+
+    await newPost.save();
+    revalidatePath("/blog");
+    console.log("saved to DB");
+  } catch (error) {
+    console.log("error", error);
+    return { error: "Something went wrong" };
+  }
+};
+
+export const deletePost = async (formData) => {
+  const { id } = Object.fromEntries(formData);
+  console.log("id", id);
+  try {
+    connectToDb();
+
+    await Post.findByIdAndDelete(id);
+    revalidatePath("/blog");
+    console.log("deleted from DB");
+  } catch (error) {
+    console.log("error", error);
+    return { error: "Something went wrong" };
+  }
+};
