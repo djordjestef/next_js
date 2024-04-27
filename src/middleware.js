22b8c6d1e33de9c806bb9593935fb1e25378cdd4
djordje <djordje.stefanovic89@gmail.com>
2024-04-27 20:07:46 +0200

    commented code

--- src/middleware.js
+++ src/middleware.js
@@ -7,12 +7,12 @@ export default NextAuth(authConfig).auth; //fn that call authConfig callback aut
 export const config = {
   matcher: [
     /*
-     * Match all request paths except for the ones starting with:
+     * Match all request paths except for the ones starting with:sve osim ove koje pocinju sa navesdenim dole
      * - api (API routes)
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|register).*)",
   ],
 };
